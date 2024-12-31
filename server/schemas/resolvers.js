6cf3da53ec485d43d58ff316528b4a53d694f2f3
Zakary Johnson <zakary.johnson575@gmail.com>
2024-12-31 10:27:52 -0600

    added the add user and login mutation

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -16,6 +16,30 @@ const resolvers = {
         
         throw new AuthenticationError('Not logged in');
       }         
+    },
+    Mutation: {
+        addUser: async (_, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+          
+            return { token, user };
+        },
+        login: async (_, { email, password }) => {
+            const user = await User.findOne({ email });
+          
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const correctPw = await user.isCorrectPassword(password);
+          
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+          
+            const token = signToken(user);
+            return { token, user };
+        }
     }
 
 };
